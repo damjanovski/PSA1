d7597826ddd4a09c4b9ad92ba7b540328e02f08d
damjanovski <damjanovski.darko@yahoo.com>
2021-12-13 01:40:19 +0100

    First commit

---
+++ Projekat/Controllers/SiteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekat.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Site
+        public ActionResult Pocetna()
+        {
+            return View();
+        }
+
+        public ActionResult Onama()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+    }
+}
